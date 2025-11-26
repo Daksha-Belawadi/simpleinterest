a1c1626d6a64d9591b8cf9782b1ca9008ab655c1
Daksha-Belawadi <belawadidaksha@gmail.com>
2025-11-26 21:06:27 +0530

    Create simple.py

---
+++ simple.py
@@ -0,0 +1,12 @@
+if len(sys.argv) == 4:
+ principal = sys.argv[1]
+ rate = sys.argv[2]
+ time = sys.argv[3]
+ print("User provided input values:")
+else:
+ print("No input given - using default values:")
+ principal = "1000"
+ rate = "5"
+ time = "2"
+# Calculation
+simple_interest = (eval(principal) * eval(rate) * eval(time)) / 100
